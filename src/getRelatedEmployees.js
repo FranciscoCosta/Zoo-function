956f9b5a8034fa23dce050259abb8f561afc8d4d
Francisco Costa <francisco100eg@gmail.com>
2022-07-29 15:45:35 -0300

    COrrigindo erros lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id));
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const funcionario = data.employees.filter((manager) =>
+      manager.managers.includes(managerId));
+    const nomes = funcionario.map((element) => `${element.firstName} ${element.lastName}`);
+    return nomes;
+  }
 }
-
+getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83');
 module.exports = { isManager, getRelatedEmployees };
