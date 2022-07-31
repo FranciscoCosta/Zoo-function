281516a1dba6d1d111dcfeaae93cddb63b3ae3eb
Francisco Costa <francisco100eg@gmail.com>
2022-07-31 14:56:27 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionarios = data.employees.find((funcionario) => funcionario.id === id);
+  const especieID = funcionarios.responsibleFor[0];
+  const especie = data.species.find((specie) => specie.id === especieID);
+  const velho = especie.residents.reduce((acc, curr) => {
+    if (curr.age > acc.age) return curr;
+    return acc;
+  });
+  return [velho.name, velho.sex, velho.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
